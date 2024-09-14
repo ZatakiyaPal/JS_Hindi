950eafb034b9ed13c3ffe05a47689f8154b7cff1
ZatakiyaPal <palzatakiya5691@gmail.com>
2024-09-14 11:43:14 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// const isUserLogedIn = true
+// const temperature = 41
+// if (temperature === 41) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50"); 
+// }
+// console.log("execute");
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power: ${power}`); 
+// }
+// console.log(`user power: ${power}`); 
+
+// const balance = 700
+
+// if (balance < 500) {
+//     console.log("less than 500");
+    
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 900");
+
+// } else {
+//     console.log("less than 1200");
+// }    
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitcard && 2==3) {
+    console.log("Allow to buy course");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
